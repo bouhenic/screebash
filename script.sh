e9d54eb7979c579fdddfaaa62643f73e548f012d
bouhenic <39024540+bouhenic@users.noreply.github.com>
2024-12-03 11:13:18 +0100

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+
+###############################################################
+#  TITRE: 
+#
+#  AUTEUR:   Samuel
+#  VERSION: 
+#  CREATION:  
+#  MODIFIE: 
+#
+#  DESCRIPTION: 
+###############################################################
+
+
+echo "clear" >> /home/ciel/.bashrc
+echo "echo -e '\\033[0;32m'" >> /home/ciel/.bashrc
+echo "echo \"███╗   ██╗███████╗██╗    ██╗████████╗ ██████╗ ███╗   ██╗\"" >> /home/ciel/.bashrc
+echo "echo \"████╗  ██║██╔════╝██║    ██║╚══██╔══╝██╔═══██╗████╗  ██║\"" >> /home/ciel/.bashrc
+echo "echo \"██╔██╗ ██║█████╗  ██║ █╗ ██║   ██║   ██║   ██║██╔██╗ ██║\"" >> /home/ciel/.bashrc
+echo "echo \"██║╚██╗██║██╔══╝  ██║███╗██║   ██║   ██║   ██║██║╚██╗██║\"" >> /home/ciel/.bashrc
+echo "echo \"██║ ╚████║███████╗╚███╔███╔╝   ██║   ╚██████╔╝██║ ╚████║\"" >> /home/ciel/.bashrc
+echo "echo \"╚═╝  ╚═══╝╚══════╝ ╚══╝╚══╝    ╚═╝    ╚═════╝ ╚═╝  ╚═══╝\"" >> /home/ciel/.bashrc
